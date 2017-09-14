9b5bb1452d9cef811df2b160f8288151a5518e77
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-09-13 21:20:58 -0300

    Exemplos diversos para GET

--- exemplos-spring-jar/src/main/java/br/senac/tads4/dsw/exemplosspringjar/controller/ExemploController.java
+++ exemplos-spring-jar/src/main/java/br/senac/tads4/dsw/exemplosspringjar/controller/ExemploController.java
@@ -11,8 +11,10 @@ import java.util.Date;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -30,13 +32,44 @@ public class ExemploController {
   @RequestMapping("/xpto2")
   public String executar2(
           @RequestParam("nome") String nome,
-          @RequestParam("idade") int idade,
+          @RequestParam(value = "idade", required = false) Integer idade,
           Model modelo) {
     modelo.addAttribute("nomeAttr", nome);
     modelo.addAttribute("idadeAttr", idade);
     return "exemplo2";
   }
-  
+
+  @RequestMapping("/xpto3/{nomePessoa}")
+  public String executar3(
+          @PathVariable("nomePessoa") String nome,
+          @RequestParam(value = "idade", required = false) Integer idade,
+          Model modelo) {
+    modelo.addAttribute("nomeAttr", nome);
+    modelo.addAttribute("idadeAttr", idade);
+    return "exemplo2";
+  }
+
+  @RequestMapping("/xpto3mv/{nomePessoa}")
+  public ModelAndView executar3MV(
+          @PathVariable("nomePessoa") String nome,
+          @RequestParam(value = "idade", required = false) Integer idade) {
+    ModelAndView resposta = new ModelAndView("exemplo2");
+    resposta.addObject("nomeAttr", nome);
+    resposta.addObject("idadeAttr", idade);
+    return resposta;
+  }
+
+  @RequestMapping("/formatadores")
+  public ModelAndView formatadores() {
+    ModelAndView resposta = new ModelAndView("formatadores");
+    resposta.addObject("dataHora", new Date());
+    resposta.addObject("moeda", new BigDecimal(3.14));
+    resposta.addObject("booleanoV", true);
+    resposta.addObject("booleanoF", false);
+    resposta.addObject("valorAleatorio", System.currentTimeMillis() % 3);
+    return resposta;
+  }
+
   @RequestMapping("/produtos")
   public String listarProdutos(Model modelo) {
     List<Produto> produtos = new ArrayList<>();
