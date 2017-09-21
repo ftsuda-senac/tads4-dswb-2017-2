0da8eb51665f65358b7c33eb8e80cd2c8323d535
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-09-20 21:38:24 -0300

    Navegação lista para detalhe

---
+++ tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/controller/ProdutoController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads4.dsw.tadsstore.controller;
+
+import br.senac.tads4.dsw.tadsstore.common.entity.Produto;
+import br.senac.tads4.dsw.tadsstore.common.service.ProdutoService;
+import br.senac.tads4.dsw.tadsstore.common.service.fakeimpl.ProdutoServiceFakeImpl;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+@Controller
+@RequestMapping("/produto")
+public class ProdutoController {
+
+  private ProdutoService service = new ProdutoServiceFakeImpl();
+
+  @RequestMapping
+  public ModelAndView listar() {
+    List<Produto> lista = service.listar(0, 100);
+    return new ModelAndView("produto/lista").addObject("itens", lista);
+  }
+
+  @RequestMapping("/{id}")
+  public ModelAndView obterPorId(@PathVariable("id") Long idProduto) {
+    Produto p = service.obter(idProduto);
+    return new ModelAndView("produto/detalhe").addObject("produto", p);
+  }
+
+}
