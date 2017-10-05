486eab7f313de1955e2d76ab18ce3d0ffe6dd413
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-10-04 22:13:21 -0300

    Implementação i18n

--- tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/config/WebMvcConfig.java
+++ tadsstore-spring/src/main/java/br/senac/tads4/dsw/tadsstore/config/WebMvcConfig.java
@@ -5,10 +5,18 @@
  */
 package br.senac.tads4.dsw.tadsstore.config;
 
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  *
@@ -22,5 +30,34 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/estaticos/**")
             .addResourceLocations("file:C:/desenv/uploads/");
+  }
+  
+  @Bean(name = "localeResolver")
+  public CookieLocaleResolver localeResolver() {
+    CookieLocaleResolver clr = new CookieLocaleResolver();
+    clr.setDefaultLocale(new Locale("pt", "BR"));
+    return clr;
+  }
+  
+  @Bean(name = "localeChangeInterceptor")
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+  
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+  
+  @Bean(name = "messageSource")
+  public ReloadableResourceBundleMessageSource messageSource() {
+    ReloadableResourceBundleMessageSource source = 
+            new ReloadableResourceBundleMessageSource();
+    source.setBasename("classpath:i18n/mensagens");
+    source.setDefaultEncoding("UTF-8");
+    source.setCacheSeconds(10);
+    return source;
   }
 }
